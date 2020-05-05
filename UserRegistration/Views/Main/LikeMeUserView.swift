eb56756ed851acaacf8779246e285ac56b49eb93
shinohara-san <someiyoshino0809@gmail.com>
2020-05-05 17:20:59 +0900

    fixed userlist view

---
+++ UserRegistration/Views/Main/LikeMeUserView.swift
@@ -0,0 +1,21 @@
+//
+//  LikeMeUserView.swift
+//  UserRegistration
+//
+//  Created by Yuki Shinohara on 2020/05/05.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct LikeMeUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LikeMeUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikeMeUserView()
+    }
+}
